4dfeb9a7049f4c5cfe3c130137e6b2f3b882a5d0
Zyq <996894913@qq.com>
2017-10-13 16:45:42 +0800

    excel表格列宽优化

--- src/main/java/com/cu/service/result/impl/ResultServiceImpl.java
+++ src/main/java/com/cu/service/result/impl/ResultServiceImpl.java
@@ -10,6 +10,7 @@ import org.apache.poi.ss.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -74,11 +75,11 @@ public class ResultServiceImpl implements ResultService {
         //首先创建字体样式
         HSSFFont font1 = wb.createFont();//创建header字体样式
         font1.setFontName("微软雅黑");//使用宋体
-        font1.setFontHeightInPoints((short) 12);//字体大小
+        font1.setFontHeightInPoints((short) 10);//字体大小
         font1.setBold(true);//加粗
         HSSFFont font2 = wb.createFont();//创建数据行字体样式
         font2.setFontName("宋体");
-        font2.setFontHeightInPoints((short) 12);
+        font2.setFontHeightInPoints((short) 10);
 
         //设置header单元格样式
         HSSFCellStyle style1 = wb.createCellStyle();
@@ -108,10 +109,43 @@ public class ResultServiceImpl implements ResultService {
         //创建excel的sheet
         HSSFSheet sheet = wb.createSheet();
         //设置列的默认宽度
-        sheet.setDefaultColumnWidth(15);
+        //sheet.setDefaultColumnWidth(15);
+        for (int i = 0; i < 5; i++) {
+            //设置列宽（动态设置）
+            //计算列宽的方法（拉格朗日插值）
+            BigDecimal w =new BigDecimal(0.0);
+            BigDecimal[] a = {BigDecimal.valueOf(-0.0002919), BigDecimal.valueOf(0.016), BigDecimal.valueOf(-0.3761903),
+                    BigDecimal.valueOf(4.95658416), BigDecimal.valueOf(-40.125), BigDecimal.valueOf(205.49), BigDecimal.valueOf(-658.901),
+                    BigDecimal.valueOf(1256.365), BigDecimal.valueOf(-1265.211), BigDecimal.valueOf(503.886), BigDecimal.valueOf(4.29)};
+            BigDecimal[] x=new BigDecimal[10];
+            for (int j=0;j<10;j++){
+                x[j]=new BigDecimal(Math.pow(i,10-j));
+                x[j]=a[j].multiply(x[j]);
+            }
+            for (int k=0;k<x.length;k++){
+                w=w.add(x[k]);
+            }
+            w=w.add(a[10]);
+            sheet.setColumnWidth(i, w.intValue() * 256 + 184);
+        }
+        for (int i = 5;i<11;i++){
+            BigDecimal w =new BigDecimal(0.0);
+            BigDecimal[] a = {BigDecimal.valueOf(1.366), BigDecimal.valueOf(-50.034), BigDecimal.valueOf(720.965),
+                    BigDecimal.valueOf(-5107.990), BigDecimal.valueOf(17799.943), BigDecimal.valueOf(-24405.39)};
+            BigDecimal[] x=new BigDecimal[5];
+            for (int j=0;j<5;j++){
+                x[j]=new BigDecimal(Math.pow(i,5-j));
+                x[j]=a[j].multiply(x[j]);
+            }
+            for (int k=0;k<x.length;k++){
+                w=w.add(x[k]);
+            }
+            w=w.add(a[5]);
+            sheet.setColumnWidth(i, w.intValue() * 256 + 184);
+        }
         //设置header内容
         HSSFRow row = sheet.createRow(0);
-        for (int i =0;i<header.length;i++){
+        for (int i = 0; i < header.length; i++) {
             HSSFCell cell = row.createCell(i);
             cell.setCellStyle(style1);
             HSSFRichTextString text = new HSSFRichTextString(header[i]);
@@ -120,9 +154,9 @@ public class ResultServiceImpl implements ResultService {
 
         //遍历resultList存入excel
         int rownum = 1;//数据行从第二行开始
-        for (Result result:resultList){
+        for (Result result : resultList) {
             row = sheet.createRow(rownum);
-            List<String> rs=new ArrayList<>();
+            List<String> rs = new ArrayList<>();
             rs.add(String.valueOf(rownum));//序号
             rs.add(result.getType());//类型
             rs.add(result.getBalk_no());//受理单号
@@ -134,10 +168,10 @@ public class ResultServiceImpl implements ResultService {
             rs.add(result.getIntro());//填写内容（处理过程）
             rs.add(result.getContent_key());//申告内容关键字
             rs.add(result.getProc_key());//处理过程关键字
-            for(int i=0;i<rs.size();i++){
+            for (int i = 0; i < rs.size(); i++) {
                 HSSFCell cell = row.createCell(i);
                 cell.setCellStyle(style2);
-                HSSFRichTextString richTextString=new HSSFRichTextString(rs.get(i));
+                HSSFRichTextString richTextString = new HSSFRichTextString(rs.get(i));
                 cell.setCellValue(richTextString);
             }
             rownum++;
